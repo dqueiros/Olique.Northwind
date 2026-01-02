ccee632eddd8264e28f9bc63006c65b61e6e81f7
dqueiros <diegoqueiros@hotmail.com>
2026-01-02 20:52:22 -0300

    Add Context e configurado connection string

--- Olique.Northwind/Olique.Northwind.Api/Program.cs
+++ Olique.Northwind/Olique.Northwind.Api/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Olique.Northwind.Infrastructure.Context;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 // Add services to the container.
 
