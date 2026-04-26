e6ac24f69e7377f5cc8ff5bf618958a42b89efe1
dqueiros <diegoqueiros@hotmail.com>
2026-04-26 17:56:50 -0300

    permissao para o projeto frontend vue

--- Olique.Northwind/Olique.Northwind.Api/Program.cs
+++ Olique.Northwind/Olique.Northwind.Api/Program.cs
@@ -4,7 +4,6 @@ using Olique.Northwind.Application.Services;
 using Olique.Northwind.Domain.Interfaces;
 using Olique.Northwind.Infrastructure.Context;
 using Olique.Northwind.Infrastructure.Repositories;
-using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,34 +12,29 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString));
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAngular",
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:4200")
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
+    options.AddPolicy("FrontendPolicy", policy =>
+    {
+        policy.WithOrigins(
+            "http://localhost:5173",
+            "http://localhost:4200"
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
 });
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Application
 builder.Services.AddScoped<ICategoryService, CategoryService>();
-
-// Infrastructure (Repository)
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -49,7 +43,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseCors("AllowAngular");
+app.UseCors("FrontendPolicy");
 
 app.UseAuthorization();
 
